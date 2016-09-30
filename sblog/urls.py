2cf193ab92e9897ddb2e5bf265f4a69df9f2663a
dlwxxxdlw <dlwxxxdlw@163.com>
2016-09-30 17:00:57 +0800

    blog_add.html
    增加添加的页面

--- sblog/urls.py
+++ sblog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^bloglist/',sblog_views.blog_list,name='bloglist'),
     url(r'^blog/(?P<id>\d+)/$',sblog_views.blog_show,name = "detailblog"),
     url(r'^blog/(?P<id>\w+)/del/$',sblog_views.blog_del,name="delblog"),
+    url(r'^blog/tag/(?P<id>\d+)/$',sblog_views.blog_filter,name="filterblog"),
+    url(r'^blog/add/$',sblog_views.blog_add,name="addblog"),
 ]
--- sblog/views.py
+++ sblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import Http404
 
-from .models import Blog
+from .models import Blog,Tag,Author
+from .forms import BlogForm
 
 # Create your views here.
 
@@ -26,3 +27,25 @@ def blog_del(request,id=""):
         return  redirect("/sblog/bloglist/")
     blogs = Blog.objects.all()
     return render(request,"sblog/blog_list.html",{"blogs":blogs})
+
+def blog_filter(request,id=""):
+    tags = Tag.objects.all()
+    tag = Tag.objects.get(id = id)
+    blogs = tag.blog_set.all()
+    return  render(request,"sblog/blog_filter.html",{"blogs":blogs,"tag":tag,"tags":tag})
+
+def blog_add(request,id=""):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            title = cd['caption']
+            author = Author.objects.get(id=id)
+            content = cd['content']
+            blog = Blog(caption=title,author=author,content=content)
+            blog.save()
+            id = Blog.objects.order_by('-publish_time')[0].id
+            return redirect("/sblog/blog/%s" % id)
+    else:
+        form = BlogForm()
+    return render(request,"sblog/blog_add.html",{'form':form})
